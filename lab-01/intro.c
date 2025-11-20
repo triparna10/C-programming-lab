55dc5515d2c6b46de99a4fd3833d4ac2aa2c4de8
triparna10 <mondalnripendra039@gmail.com>
2025-11-20 22:18:52 +0530

    Add introductory program in intro.c

---
+++ lab-01/intro.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("----- My Introduction -----\n");
+    printf("Name     : Triparna Mondal\n");
+    printf("Dept   : Mechanical\n");
+    printf("Roll No  : MEB25062\n");
+    printf("INSTITUTE   : TEZPUR UNIVERSITY\n");
+    printf("Hobbies  : Watching films, Playing Football\n");
+
+    return 0;
+}
