a29dc0d4c01c5b7348a1c2f1785c541465cb2e46
triparna10 <mondalnripendra039@gmail.com>
2025-11-20 22:22:56 +0530

    Add tensile strength comparison program

--- lab-03/tensilestrength-compare.c
+++ lab-03/tensilestrength-compare.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    float t1, t2;
+    printf("Enter tensile strength of material 1 (MPa): ");
+    scanf("%f", &t1);
+    printf("Enter tensile strength of material 2 (MPa): ");
+    scanf("%f", &t2);
+
+    if (t1 > t2)
+        printf("Material 1 has higher tensile strength: %.2f MPa\n", t1);
+    else if (t2 > t1)
+        printf("Material 2 has higher tensile strength: %.2f MPa\n", t2);
+    else
+        printf("Both materials have equal tensile strength: %.2f MPa\n", t1);
+
+    return 0;
+}
