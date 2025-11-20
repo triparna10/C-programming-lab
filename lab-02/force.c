158aed1c9cc5dd9941a250f27f08a3781c58b94f
triparna10 <mondalnripendra039@gmail.com>
2025-11-20 22:19:36 +0530

    Update force.c

--- lab-02/force.c
+++ lab-02/force.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    float mass, acceleration, force;
+    printf("Enter mass (kg): ");
+    scanf("%f", &mass);
+    printf("Enter acceleration (m/s^2): ");
+    scanf("%f", &acceleration);
+
+    force = mass * acceleration;
+    printf("Force = %.2f N\n", force);
+
+    return 0;
+}
